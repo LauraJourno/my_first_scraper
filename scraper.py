5248077ca420d0ce40e8e2afd1b3d7251f8147ac
Laura Sanders Journalism <43606354+LauraJourno@users.noreply.github.com>
2019-02-19 11:45:14 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,11 +8,18 @@ print("hello")
 # # Read in a page
 html = scraperwiki.scrape("http://foo.com")
 print(html)
-#
+# EXPLANATION: THE ABOVE CODE HAS A VARIABLE WHICH IS HTML, THE FUNCTION .scrape AND THE ("STRING"). 
+# SO THE CODE HAS GONE TO THIS SITE AND SCRAPED THE HTML WITHIN THE STRING. WE RAN THIS ON MORPH.IO. 
+# NOW, WE WANT TO BE MORE SPECIFIC, IT HAS JUST SCRAPED THE WHOLE PAGE. SO WE ARE GOING TO DRILL DOWN FURTHER.
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
+root = lxml.html.fromstring(html)
+print(root.cssselect("div[align='left']"))
+#ROOT = HAS CREATED A NEW VARIABLE. THE WORD ROOT IS COMPLETELY ARBITUARY, YOU COULD NAME IS ANYTHING.
+#LXML.HTML is a library we have imported. It is a library which allows you to convert webpages.
+#FROMSTRING - IS A FUNCTION WHICH CONVERTS SOMETHING FROM A STRING OF CHARACTERS IN TO SOMETHING THAT CAN BE DRILLED DOWN IN TO.
+#THE STRING FROM LXML.HTML WILL THEN BE MADE IN TO A NEW VARIABLE UNDER ROOT.
+#CSSSELECT IS A FUNCTION TO DRILL DOWN IN TO THE CODE ON THE PAGE. THIS LINE OF CODE IS ASKING IT TO MAKE A MATCH TO ANY HTML TAGS WITIN THE STRING THAT MATCH THAT CRITERIA.
+#SO "DIVALIGN-'LEFT'... DIV AND ALIGN LAFT ARE HTML TAGS, ALIGN LEFT IS AN ATTRIBUTE, SO WE'RE ASKING IT TO GRAB ANYTHING THAT MATCHES THIS. 
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
